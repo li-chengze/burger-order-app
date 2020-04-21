e0329333277e7df29250ceeaf8633cbebbc9d5f5
Chengze Li <asd13518662@fb.com>
2020-04-21 22:39:02 +0800

    add spiner

---
+++ src/components/UI/Spiner/Spiner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Spiner.module.css';
+
+const spiner = () => {
+    return <div className={classes.Loader}>Loading...</div>
+}
+
+export default spiner;
