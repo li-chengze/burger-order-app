ffd218e6b610ee1adab86be5e92f9fb3fb194127
Chengze Li <asd13518662@fb.com>
2020-04-22 16:07:05 +0800

    build the whole project

---
+++ build/precache-manifest.b8546b62ec35dc59156b429108355020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d3a352c8783d80c08ae316d5956299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69f893b44a39578d787e",
+    "url": "/static/css/main.303ff343.chunk.css"
+  },
+  {
+    "revision": "671867dc097e80de46bf",
+    "url": "/static/js/2.e52a0a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e52a0a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f893b44a39578d787e",
+    "url": "/static/js/main.09527332.chunk.js"
+  },
+  {
+    "revision": "a18e57e49ed1abfe25e7",
+    "url": "/static/js/runtime-main.87c4d2f9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/logo.b8503d26.png"
+  }
+]);
