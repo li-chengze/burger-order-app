216661652c7867ec18b80b677c94eaf50b52973d
Chengze Li <asd13518662@fb.com>
2020-04-22 16:15:40 +0800

    Updates

---
+++ precache-manifest.52d8792d1b0650990b9d5fa7cb5d8a15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb990dc5b035b979a9a8ccc34158da2c",
+    "url": "/li-chengze/burger-order-app/index.html"
+  },
+  {
+    "revision": "f15b4c310e80e58cbcbd",
+    "url": "/li-chengze/burger-order-app/static/css/main.303ff343.chunk.css"
+  },
+  {
+    "revision": "671867dc097e80de46bf",
+    "url": "/li-chengze/burger-order-app/static/js/2.e52a0a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/li-chengze/burger-order-app/static/js/2.e52a0a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15b4c310e80e58cbcbd",
+    "url": "/li-chengze/burger-order-app/static/js/main.3dccf2a6.chunk.js"
+  },
+  {
+    "revision": "80babd0938bf3de7d3ed",
+    "url": "/li-chengze/burger-order-app/static/js/runtime-main.d292c323.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/li-chengze/burger-order-app/static/media/logo.b8503d26.png"
+  }
+]);
